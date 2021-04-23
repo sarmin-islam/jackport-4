9cdae69d3d5e1c6bff6551ee063517fff830167b
sarmin-islam <mony.eden@gamil.com>
2021-04-24 02:54:08 +0600

    jackport-4

---
+++ js/custom.js
@@ -0,0 +1,91 @@
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  let birthday = "Sep 30, 2021 00:00:00",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+      //   document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown"),
+              content = document.getElementById("content");
+
+          headline.innerText = "It's my birthday!";
+          countdown.style.display = "inline-block";
+          content.style.display = "inline-block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
+
+    //Tounament scroll js
+    $(".scorllbar").niceScroll({
+        cursorcolor: "#1f004f",
+        cursorwidth: "10px",
+        cursorborder: "transparent",
+        autohidemode: false,
+         horizrailenabled:false,
+
+
+        iframeautoresize: true,
+        smoothscroll: true,
+        });
+
+
+            
+//back to top, fixed-nav
+
+$(function(){
+  var backToTop = $('.back-to-top');
+  var html_body = $('html,body');
+  
+  
+      $(window).scroll(function () { 
+          var scrolling = $(this).scrollTop();
+          if(scrolling > 200){
+              backToTop.fadeIn();
+          }else{
+              backToTop.fadeOut();
+          }
+          if(scrolling >200){
+               $('.navbar').addClass('fixed-nav');
+           }else{
+               $('.navbar').removeClass('fixed-nav');
+           }
+      });
+  
+  
+      backToTop.on('click',function(){
+          html_body.animate({
+              scrollTop:0,
+          },1500);
+      });
+  
+      $('.navbar .navbar-nav .nav-item .nav-link').on('click', function () {
+          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              if (target.length) {
+                  html_body.animate({
+                      scrollTop: target.offset().top - 0
+                  }, 1500,);
+                  return false;
+              }
+          }
+      });
+      
+  });
